2b5f6282f46d760502bf0e9493641ad9a3fe9686
André Gervásio <andreger@gmail.com>
2025-04-28 23:21:36 -0300

    Change database to memory

--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -1,5 +1,4 @@
 import { DataSource } from "typeorm";
-import path from "path";
 import { Topic } from "../../topic/entities/topic.entity";
 import { TopicHistory } from "../../topic/entities/topic-history.entity";
 import { User } from "../../user/entities/user.entity";
@@ -7,8 +6,7 @@ import { Resource } from "../../resource/entities/resource.entity";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  // database: ":memory:",
-  database: path.join(__dirname, "../../../database.sqlite"),
+  database: ":memory:",
   synchronize: true,
   logging: true,
   entities: [Resource, Topic, TopicHistory, User],
